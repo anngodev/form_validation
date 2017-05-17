39807121ac201ae84ebe521aaf9354e4ee911dc8
An Ngo <ngoan_230@yahoo.com>
2017-05-17 05:31:04 -0400

    success message and booleanstatements

--- assets/javascripts/formValidation.js
+++ assets/javascripts/formValidation.js
@@ -5,18 +5,46 @@ function isEmail(email) {
 
 $("#submitButton").click(function() {
 	var errorMessage = "";
+	var fieldMissing = "";
+
+	if ($("#email").val() == "") {
+		fieldMissing += "<br>*E-mail";
+	}
+
+	if ($("#phone").val() == "") {
+		fieldMissing += "<br>*Phone";
+	}
+
+	if ($("#password").val() == "") {
+		fieldMissing += "<br>*Password";
+	}
+
+	if ($("#passwordConfirm").val() == "") {
+		fieldMissing += "<br>*Confirm Password";
+	}
+
+	if (fieldMissing != "") {
+		errorMessage += "<p>The following field(s) are missing!" + fieldMissing;
+	}
 
 	if (isEmail($("#email").val()) == false) {
-		errorMessage += "<p>Your E-mail address is not valid</p>";
+		errorMessage += "<p>*Your E-mail address is not valid</p>";
 	}
 
 	if ($.isNumeric($("#phone").val()) == false) {
-		errorMessage += "<p>Your phone number is not valid</p>"
+		errorMessage += "<p>*Your phone number is not valid</p>"
 	}
 
 	if ($("#password").val() != $("#passwordConfirm").val()) {
-		errorMessage += "<p>Sorry, your password does not match</p>"
+		errorMessage += "<p>*Sorry, your password does not match</p>"
 	}
 
-	alert(errorMessage);
+	if (errorMessage != "") {
+		$("#errorMessage").html(errorMessage);
+
+	} else {
+		$("#successMessage").show();
+		$("#errorMessage").hide();
+	}
+
 });
