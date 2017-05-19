5d430f5233d02d0cce38df6e574fcc48fc7e479c
An Ngo <ngoan_230@yahoo.com>
2017-05-19 00:30:32 -0400

    resizable with callback function

--- assets/javascripts/jqueryUI.js
+++ assets/javascripts/jqueryUI.js
@@ -1,2 +1,13 @@
 $("#draggable").draggable({axis: "y"});
 $( "span" ).draggable({ containment: "parent" });
+$("#resizable").resizable({
+	grid: 50,
+	alsoResize: "#also",
+	resize: function(event, ui) {
+		if ($("#resizable").width() > 300) {
+			alert("big enough");
+		}
+	}
+
+});
+	$("#also").resizable();
